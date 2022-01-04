3e59fa26846220b58db40f4bf4adca7121fd2b33
JasonLiang5129 <94155740+JasonLiang5129@users.noreply.github.com>
2022-01-03 19:46:44 -0800

    more xml changes

---
+++ app/src/main/java/com/example/billsplit_app/IndividualBillScreen.java
@@ -0,0 +1,14 @@
+package com.example.billsplit_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IndividualBillScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.individual_bill_screen);
+    }
+}
---
+++ app/src/main/java/com/example/billsplit_app/WelcomeScreen.java
@@ -0,0 +1,16 @@
+package com.example.billsplit_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_screen);
+//
+//        hi
+    }
+}
