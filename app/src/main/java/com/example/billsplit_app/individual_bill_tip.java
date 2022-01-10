f97ddd849d379451d575c4d6ec086082e1d62205
JasonLiang5129 <94155740+JasonLiang5129@users.noreply.github.com>
2022-01-09 20:34:41 -0800

    minor changes

---
+++ app/src/main/java/com/example/billsplit_app/individual_bill_tip.java
@@ -0,0 +1,14 @@
+package com.example.billsplit_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class individual_bill_tip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.individual_bill_tip);
+    }
+}
